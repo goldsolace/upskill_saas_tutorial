a717dd522f96e06ac36a094c0cbd1e0721665cd9
Brice Purton <goldsolace@gmail.com>
2017-09-22 08:04:01 +0000

    Added helpful copy

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,26 @@ module UsersHelper
       "<i class='fa fa-dollar'></i>".html_safe
     end
   end
+  
+  def job_title_icon_index(title)
+    @job_title = title[:title.to_sym]
+    if @job_title == "Developer"
+      "<i class='fa fa-code'></i>".html_safe
+    elsif @job_title == "Entrepreneur"
+      "<i class='fa fa-lightbulb-o'></i>".html_safe
+    elsif @job_title == "Investor"
+      "<i class='fa fa-dollar'></i>".html_safe
+    end
+  end
+  
+    def job_title_bg(title)
+    @job_title = title[:title.to_sym]
+    if @job_title == "Developer"
+      "developer-bg".html_safe
+    elsif @job_title == "Entrepreneur"
+      "entrepreneur-bg".html_safe
+    elsif @job_title == "Investor"
+      "investor-bg".html_safe
+    end
+  end
 end
