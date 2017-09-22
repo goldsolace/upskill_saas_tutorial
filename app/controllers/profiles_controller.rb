a88f90d7ec078251102ff73dbb9c5e738cedb0ca
Brice Purton <goldsolace@gmail.com>
2017-09-22 07:07:43 +0000

    Added before functions to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,7 @@
 class ProfilesController < ApplicationController
-    
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
   # GET to /users/:user_id/profile/new
   def new
     # Create blank profile details form
@@ -13,14 +15,40 @@ class ProfilesController < ApplicationController
     # Create profile linked to this specific user
     @profile = @user.build_profile( profile_params )
     if @profile.save
-      flash[:success] = "Profile updated!"
-      redirect_to user_path( params[:user_id] )
+      flash[:success] = "Profile Created!"
+      # Redirect user to their profile page
+      redirect_to user_path(id: params[:user_id] )
     else
       render action: :new
     end
   end
+  
+  # GET to /users/:user_id/profile/edit
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+  end
+  
+  # PATCH to /users/:user_id/profile
+  def update
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    if @profile.update_attributes( profile_params )
+      flash[:success] = "Profile Updated!"
+      # Redirect user to their profile page
+      redirect_to user_path(id: params[:user_id] )
+    else
+      render action: :edit
+    end
+  end
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   # GET to /users/:id
   def show
+    @user = User.find( params[:id] )
+  end
+  
+  # GET to /users/:id
+  def edit
     @user = User.find( params[:id] )
   end
 end
